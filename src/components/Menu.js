b65a1d2727cfb06f9286a0b91676b57efbdfebce
innovis <innovislab@gmail.com>
2019-03-21 16:20:14 +0800

    Add Main UI

---
+++ src/components/Menu.js
@@ -0,0 +1,25 @@
+const Menu = [
+    {
+      label: "Home",
+      pathname: "/"
+    },
+    {
+      label: "Dashboard",
+      pathname: "/"
+    },
+    {
+      label: "Signup",
+      pathname: "/"
+    },
+    {
+      label: "Wizard",
+      pathname: "/"
+    },
+    {
+      label: "Cards",
+      pathname: "/"
+    }
+  
+  ];
+  
+  export default Menu;
