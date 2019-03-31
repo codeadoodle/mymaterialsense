a75b39d49bebae1b5bf26d03f12c3091f3c928e5
innovis <innovislab@gmail.com>
2019-03-31 22:19:48 +0800

    Fix typography error message

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const theme = createMuiTheme({
     }
   },
   typography: {
+    useNextVariants: true,
     // Use the system font instead of the default Roboto font.
     fontFamily: [
       '"Lato"',
